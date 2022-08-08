f495756a8f14eb332f5369be9e6171640b831db8
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-08-08 14:15:02 +0900

    Add files via upload

---
+++ Note.py
@@ -0,0 +1,60 @@
+Python
+    https://www.python.org/downloads/
+    python-?.?.?-amd64.exe
+
+Visual Studio Code
+    https://code.visualstudio.com/
+    VSCodeUserSetup-x64-?.?.?.exe
+    Python
+    Japanese Language Pack for Visual Studio Code
+
+GitHub
+    https://github.com/
+
+コメント
+    # コメント
+
+型
+    整数
+    小数
+    “文字列”
+
+変数
+    変数 = 値
+    変数
+    配列 = [値]
+    配列
+    配列[インデックス]
+    連想配列 = {キー:値}
+    連想配列
+    連想配列[キー]
+
+演算子
+    + - * ** / // %
+    == != < <= > >=
+    and or not
+
+条件分岐
+    if 条件:
+        処理
+    elif 条件:
+        処理
+    else:
+        処理
+
+繰り返し
+    for 変数 in 配列:
+        処理
+        continue
+        break
+
+    while 条件:
+        処理
+        continue
+        break
+
+ライブラリ
+    pip list ライブラリ
+    pip install ライブラリ
+    import ライブラリ
+    from ライブラリ import *
